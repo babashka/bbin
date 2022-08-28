0fb86947b16e844f3399ad0b1c48760f4ddbc5f1
Radford Smith <11401+rads@users.noreply.github.com>
2022-08-27 19:11:38 -0700

    Move tests to `babashka.bbin.scripts-test`

--- test/babashka/bbin/scripts_test.clj
+++ test/babashka/bbin/scripts_test.clj
@@ -1,9 +1,12 @@
 (ns babashka.bbin.scripts-test
-  (:require [clojure.test :refer [deftest is]]
-            [babashka.bbin.test-util :refer [bbin-root reset-test-dir]]
+  (:require [clojure.test :refer [deftest is testing]]
+            [babashka.bbin.test-util :refer [bbin-root reset-test-dir test-dir]]
             [babashka.bbin.scripts :as scripts]
             [babashka.fs :as fs]
-            [babashka.bbin.util :as util]))
+            [babashka.process :refer [sh]]
+            [babashka.bbin.util :as util]
+            [clojure.string :as str]
+            [clojure.edn :as edn]))
 
 (def bbin-test-lib
   '{:lib io.github.rads/bbin-test-lib,
@@ -26,3 +29,103 @@
                        :git/tag "v0.0.1",
                        :git/sha "9140acfc12d8e1567fc6164a50d486de09433919"}}}
            (scripts/load-scripts cli-opts)))))
+
+(def test-lib
+  {:lib 'io.github.rads/bbin-test-lib
+   :coords {:git/url "https://github.com/rads/bbin-test-lib"
+            :git/tag "v0.0.1"
+            :git/sha "9140acfc12d8e1567fc6164a50d486de09433919"}})
+
+(def portal-script-url
+  (str "https://gist.githubusercontent.com"
+       "/rads/da8ecbce63fe305f3520637810ff9506"
+       "/raw/e83305656f2d145430085d5414e2c3bff776b6e8/portal.clj"))
+
+(defn run-install [parsed-args]
+  (some-> (with-out-str (scripts/install parsed-args))
+          edn/read-string))
+
+(defn run-bin-script [script-name & script-args]
+  (let [args (cons (str "bin/" (name script-name)) script-args)
+        {:keys [out]} (sh args {:dir bbin-root :err :inherit})]
+    (str/trim out)))
+
+(deftest install-from-qualified-lib-name-test
+  (testing "install */*"
+    (reset-test-dir)
+    (util/ensure-bbin-dirs {:bbin/root bbin-root})
+    (let [cli-opts {:script/lib "io.github.rads/bbin-test-lib"
+                    :bbin/root bbin-root}
+          out (run-install {:opts cli-opts})
+          bin-file (fs/file bbin-root "bin/hello")]
+      (is (= test-lib out))
+      (is (fs/exists? bin-file))
+      (is (= "Hello world!" (run-bin-script 'hello))))))
+
+(def maven-lib
+  {:lib 'org.babashka/http-server
+   :coords {:mvn/version "0.1.11"}})
+
+(deftest install-from-mvn-version-test
+  (testing "install */* --mvn/version *"
+    (reset-test-dir)
+    (util/ensure-bbin-dirs {:bbin/root bbin-root})
+    (let [cli-opts {:script/lib (str (:lib maven-lib))
+                    :mvn/version (-> maven-lib :coords :mvn/version)
+                    :bbin/root bbin-root}
+          out (run-install {:opts cli-opts})]
+      (is (= maven-lib out))
+      (is (fs/exists? (fs/file bbin-root "bin" (name (:lib maven-lib)))))
+      (is (str/starts-with? (run-bin-script (:lib maven-lib) "--help")
+                            "Serves static assets using web server.")))))
+
+(deftest install-from-local-root-dir-test
+  (testing "install ./"
+    (reset-test-dir)
+    (util/ensure-bbin-dirs {:bbin/root bbin-root})
+    (let [local-root (str (fs/file test-dir "foo"))]
+      (fs/create-dir local-root)
+      (spit (fs/file local-root "bb.edn") (pr-str {}))
+      (spit (fs/file local-root "deps.edn") (pr-str {}))
+      (let [cli-opts {:script/lib "babashka/foo"
+                      :local/root local-root
+                      :bbin/root bbin-root}
+            out (run-install {:opts cli-opts})]
+        (is (= {:lib 'babashka/foo
+                :coords {:local/root local-root}}
+               out))
+        (is (fs/exists? (fs/file bbin-root "bin/foo")))))))
+
+(deftest install-from-local-root-clj-test
+  (testing "install ./*.clj"))
+
+(deftest install-from-local-root-jar-test
+  (testing "install ./*.jar"))
+
+(deftest install-from-url-clj-test
+  (testing "install https://*.clj"
+    (reset-test-dir)
+    (util/ensure-bbin-dirs {:bbin/root bbin-root})
+    (let [cli-opts {:script/lib portal-script-url
+                    :bbin/root bbin-root}
+          out (run-install {:opts cli-opts})]
+      (is (= {:coords {:bbin/url portal-script-url}} out))
+      (is (fs/exists? (fs/file bbin-root "bin/portal"))))))
+
+(deftest install-from-url-jar-test
+  (testing "install https://*.jar"))
+
+(deftest uninstall-test
+  (testing "uninstall foo"
+    (reset-test-dir)
+    (util/ensure-bbin-dirs {:bbin/root bbin-root})
+    (let [script-file (fs/file bbin-root "bin/foo")]
+      (spit script-file "#!/usr/bin/env bb")
+      (let [cli-opts {:script/lib "foo"
+                      :bbin/root bbin-root}
+            out (str/trim (with-out-str (scripts/uninstall {:opts cli-opts})))]
+        (is (= (str "Removing " script-file) out))
+        (is (not (fs/exists? script-file)))))))
+
+(comment
+  (clojure.test/run-tests))
