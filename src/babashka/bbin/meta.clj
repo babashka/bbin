2ab0cf9f502a578fe2a10f9374a1c73a3477b924
Radford Smith <11401+rads@users.noreply.github.com>
2022-09-05 21:08:38 -0700

    Fix name in comment

--- src/babashka/bbin/meta.clj
+++ src/babashka/bbin/meta.clj
@@ -1,6 +1,6 @@
 (ns babashka.bbin.meta)
 
 ;; This is a fallback if the var hasn't already been set earlier by a build
-;; script, such as when neil is called as a library instead of a script.
+;; script, such as when bbin is called as a library instead of a script.
 (defonce min-bb-version :version-not-set)
 (defonce version :version-not-set)
