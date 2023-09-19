c3ca74aa2abcc7f79fd021426fdb4f36672b75b7
Radford Smith <11401+rads@users.noreply.github.com>
2023-09-19 11:40:59 -0700

    Write log file during migration

--- test/babashka/bbin/migrate_test.clj
+++ test/babashka/bbin/migrate_test.clj
@@ -26,11 +26,12 @@
 
     (testing "scenario: no changes needed"
       (reset-test-dir)
-      (is (= [[:up-to-date]]
-             (->> (migrate/migrate :auto {:edn true})
-                  (with-in-str "")
-                  with-out-str
-                  parse-edn-out))))
+      (let [commands [[:up-to-date]]]
+        (is (= commands
+               (->> (migrate/migrate :auto {:edn true})
+                    (with-in-str "")
+                    with-out-str
+                    parse-edn-out)))))
 
     (testing "scenario: changes needed, user declines"
       (reset-test-dir)
@@ -39,16 +40,21 @@
                           (spit "#!/usr/bin/env bb\n(println \"Hello world\")"))]
         (with-out-str (scripts/install {:script/lib (str (fs/canonicalize test-script))}))
         (fs/create-dirs (dirs/legacy-bin-dir))
-        (let [parsed-script (scripts/parse-script
-                              (slurp (fs/file (dirs/legacy-bin-dir) "hello")))]
-          (is (= [[:printable-scripts {:scripts {'hello parsed-script}}]
-                  [:found-scripts]
-                  [:prompt-move]
-                  [:canceled]]
-                 (->> (migrate/migrate :auto {:edn true})
-                      (with-in-str "no\n")
-                      with-out-str
-                      parse-edn-out))))))
+        (binding [util/*now* (Instant/ofEpochSecond 123)]
+          (let [parsed-script (scripts/parse-script
+                                (slurp (fs/file (dirs/legacy-bin-dir) "hello")))
+                commands [[:printable-scripts {:scripts {'hello parsed-script}}]
+                          [:found-scripts]
+                          [:prompt-move]
+                          [:canceled]]]
+            (is (= commands
+                   (->> (migrate/migrate :auto {:edn true})
+                        (with-in-str "no\n")
+                        with-out-str
+                        parse-edn-out)))
+            (is (= commands
+                   (parse-edn-out (slurp (migrate/log-path (dirs/legacy-bin-dir)
+                                                           (inst-ms (util/now)))))))))))
 
     (testing "scenario: changes needed, user accepts, no conflicts"
       (reset-test-dir)
@@ -58,21 +64,26 @@
         (with-out-str (scripts/install {:script/lib (str (fs/canonicalize test-script))}))
         (binding [util/*now* (Instant/ofEpochSecond 123)]
           (let [parsed-script (scripts/parse-script
-                                (slurp (fs/file (dirs/legacy-bin-dir) "hello")))]
-            (is (= [[:printable-scripts {:scripts {'hello parsed-script}}]
-                    [:found-scripts]
-                    [:prompt-move]
-                    [:migrating]
-                    [:copying {:src (str (fs/file (dirs/legacy-bin-dir) "hello"))
-                               :dest (str (fs/file (dirs/xdg-bin-dir nil) "hello"))}]
-                    [:moving {:src (str (dirs/legacy-bin-dir))
-                              :dest (migrate/backup-path (dirs/legacy-bin-dir))}]
-                    [:done]]
+                                (slurp (fs/file (dirs/legacy-bin-dir) "hello")))
+                commands-1 [[:printable-scripts {:scripts {'hello parsed-script}}]
+                            [:found-scripts]
+                            [:prompt-move]
+                            [:migrating]
+                            [:copying {:src (str (fs/file (dirs/legacy-bin-dir) "hello"))
+                                       :dest (str (fs/file (dirs/xdg-bin-dir nil) "hello"))}]
+                            [:moving {:src (str (dirs/legacy-bin-dir))
+                                      :dest (migrate/backup-path (dirs/legacy-bin-dir) (inst-ms (util/now)))}]
+                            [:done]]
+                commands-2 [[:up-to-date]]]
+            (is (= commands-1
                    (->> (migrate/migrate :auto {:edn true})
                         (with-in-str "yes\n")
                         with-out-str
                         parse-edn-out)))
-            (is (= [[:up-to-date]]
+            (is (= commands-1
+                   (parse-edn-out (slurp (migrate/log-path (dirs/legacy-bin-dir)
+                                                           (inst-ms (util/now)))))))
+            (is (= commands-2
                    (->> (migrate/migrate :auto {:edn true})
                         (with-in-str "yes\n")
                         with-out-str
@@ -88,21 +99,26 @@
         (fs/copy (fs/file (dirs/legacy-bin-dir) "hello") (fs/file (dirs/xdg-bin-dir nil) "hello"))
         (binding [util/*now* (Instant/ofEpochSecond 123)]
           (let [parsed-script (scripts/parse-script
-                                (slurp (fs/file (dirs/legacy-bin-dir) "hello")))]
-            (is (= [[:printable-scripts {:scripts {'hello parsed-script}}]
-                    [:found-scripts]
-                    [:prompt-move]
-                    [:confirm-overwrite {:dest (str (fs/file (dirs/xdg-bin-dir nil) "hello"))}]
-                    [:migrating]
-                    [:skipping {:src (str (fs/file (dirs/legacy-bin-dir) "hello"))}]
-                    [:moving {:src (str (dirs/legacy-bin-dir))
-                              :dest (migrate/backup-path (dirs/legacy-bin-dir))}]
-                    [:done]]
+                                (slurp (fs/file (dirs/legacy-bin-dir) "hello")))
+                commands-1 [[:printable-scripts {:scripts {'hello parsed-script}}]
+                            [:found-scripts]
+                            [:prompt-move]
+                            [:confirm-overwrite {:dest (str (fs/file (dirs/xdg-bin-dir nil) "hello"))}]
+                            [:migrating]
+                            [:skipping {:src (str (fs/file (dirs/legacy-bin-dir) "hello"))}]
+                            [:moving {:src (str (dirs/legacy-bin-dir))
+                                      :dest (migrate/backup-path (dirs/legacy-bin-dir) (inst-ms (util/now)))}]
+                            [:done]]
+                commands-2 [[:up-to-date]]]
+            (is (= commands-1
                    (->> (migrate/migrate :auto {:edn true})
                         (with-in-str "yes\nno\n")
                         with-out-str
                         parse-edn-out)))
-            (is (= [[:up-to-date]]
+            (is (= commands-1
+                   (parse-edn-out (slurp (migrate/log-path (dirs/legacy-bin-dir)
+                                                           (inst-ms (util/now)))))))
+            (is (= commands-2
                    (->> (migrate/migrate :auto {:edn true})
                         (with-in-str "yes\n")
                         with-out-str
@@ -118,22 +134,27 @@
         (fs/copy (fs/file (dirs/legacy-bin-dir) "hello") (fs/file (dirs/xdg-bin-dir nil) "hello"))
         (binding [util/*now* (Instant/ofEpochSecond 123)]
           (let [parsed-script (scripts/parse-script
-                                (slurp (fs/file (dirs/legacy-bin-dir) "hello")))]
-            (is (= [[:printable-scripts {:scripts {'hello parsed-script}}]
-                    [:found-scripts]
-                    [:prompt-move]
-                    [:confirm-overwrite {:dest (str (fs/file (dirs/xdg-bin-dir nil) "hello"))}]
-                    [:migrating]
-                    [:copying {:src (str (fs/file (dirs/legacy-bin-dir) "hello"))
-                               :dest (str (fs/file (dirs/xdg-bin-dir nil) "hello"))}]
-                    [:moving {:src (str (dirs/legacy-bin-dir))
-                              :dest (migrate/backup-path (dirs/legacy-bin-dir))}]
-                    [:done]]
+                                (slurp (fs/file (dirs/legacy-bin-dir) "hello")))
+                commands-1 [[:printable-scripts {:scripts {'hello parsed-script}}]
+                            [:found-scripts]
+                            [:prompt-move]
+                            [:confirm-overwrite {:dest (str (fs/file (dirs/xdg-bin-dir nil) "hello"))}]
+                            [:migrating]
+                            [:copying {:src (str (fs/file (dirs/legacy-bin-dir) "hello"))
+                                       :dest (str (fs/file (dirs/xdg-bin-dir nil) "hello"))}]
+                            [:moving {:src (str (dirs/legacy-bin-dir))
+                                      :dest (migrate/backup-path (dirs/legacy-bin-dir) (inst-ms (util/now)))}]
+                            [:done]]
+                commands-2 [[:up-to-date]]]
+            (is (= commands-1
                    (->> (migrate/migrate :auto {:edn true})
                         (with-in-str "yes\nyes\n")
                         with-out-str
                         parse-edn-out)))
-            (is (= [[:up-to-date]]
+            (is (= commands-1
+                   (parse-edn-out (slurp (migrate/log-path (dirs/legacy-bin-dir)
+                                                           (inst-ms (util/now)))))))
+            (is (= commands-2
                    (->> (migrate/migrate :auto {:edn true})
                         (with-in-str "yes\n")
                         with-out-str
