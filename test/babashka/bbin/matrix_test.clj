db5a5bf699c45e6644a050f81e31a3d8f30205f3
Radford Smith <11401+rads@users.noreply.github.com>
2026-06-07 12:17:53 -0700

    Parameterize matrix bbin binary
    Change install-examples! to accept the bbin executable name explicitly and invoke it through babashka.process/shell, so the matrix helper can compare installed behavior across system bbin versions.
    
    Update the REPL comment workflow to install and run every generated example with both bbin-0.2 and bbin-0.3, then tap a Portal table with separate installed and ran columns for each version.

--- test/babashka/bbin/matrix_test.clj
+++ test/babashka/bbin/matrix_test.clj
@@ -1,10 +1,8 @@
 (ns babashka.bbin.matrix-test
-  (:require [babashka.bbin.test-util :as tu]
-            [babashka.process :as p]
+  (:require [babashka.process :as p]
             [babashka.fs :as fs]
             [clojure.edn :as edn]
-            [clojure.math.combinatorics :as combo])
-  (:import (clojure.lang ExceptionInfo)))
+            [clojure.math.combinatorics :as combo]))
 
 (def ^:private bbin-bin
   {'both-bb-edn-and-deps-edn {:main-opts ["-m" "main"]}})
@@ -60,54 +58,71 @@
        (filter fs/directory?)
        (sort-by #(str (fs/file-name %)))))
 
-(defn install-examples! [parent-dir prefix]
+(defn install-examples! [parent-dir prefix bbin-binary]
   (mapv (fn [child-dir]
           (let [basename (fs/file-name child-dir)
                 script-name (str prefix "-" basename)]
-            {:child-dir child-dir
+            {:bbin-binary bbin-binary
+             :child-dir child-dir
              :as script-name
              :input (edn/read-string (slurp (fs/file child-dir "input.edn")))
              :out (try
-                    (tu/bbin ["install" (str child-dir) "--as" script-name]
-                             :out :string)
+                    (p/shell {:out :string :err :string :continue true}
+                             bbin-binary "install" (str child-dir) "--as" script-name)
                     (catch Exception e e))}))
         (child-dirs parent-dir)))
 
 (defn run-examples! [install-results]
   (->> install-results
-       (map (fn [{:keys [as child-dir input out]}]
-              (let [error (instance? ExceptionInfo out)
-                    run (when-not error
+       (map (fn [{:keys [as bbin-binary child-dir input out]}]
+              (let [error (instance? Exception out)
+                    installed (and (not error) (zero? (:exit out)))
+                    run (when installed
                           (-> (p/shell as {:out :string :err :string :continue true})
                               (select-keys [:out :err])))]
                 {:as as
                  :child-dir (str child-dir)
                  :input input
-                 :installed (not error)
-                 :install-message (if error (ex-message out) out)
+                 :bbin-binary bbin-binary
+                 :installed installed
+                 :install-message (if error (ex-message out) (:out out))
                  :run run})))))
 
 (comment
   (def repl-inputs (take 100 (all-possible-inputs)))
   (def repl-prefix (str "test_" (System/nanoTime)))
+  (def repl-bbin-binaries ["bbin-0.2" "bbin-0.3"])
   (map-indexed vector repl-inputs)
 
   (doseq [[i input] (map-indexed vector repl-inputs)
           :let [example-dir (fs/file "target" repl-prefix (format "%02d" i))]]
     (generate-example! example-dir input))
 
-  (def install-results (install-examples! (fs/file "target" repl-prefix)
-                                          repl-prefix))
-  (def run-results (run-examples! install-results))
+  (def install-results
+    (mapcat (fn [bbin-binary]
+              (install-examples! (fs/file "target" repl-prefix)
+                                 (str repl-prefix "-" bbin-binary)
+                                 bbin-binary))
+            repl-bbin-binaries))
+  (def run-results (doall (run-examples! install-results)))
   (tap>
     (as-> run-results $
-      (map (fn [{:keys [input run installed as]}]
-             (let [ran (= (:out run) "hello world\n")]
-               {:0-as (symbol as)
-                :1-installed (if installed '✅ '❌)
-                :2-ran (if ran '✅ '❌)
-                :3-input (with-meta input {:portal.viewer/default :portal.viewer/pprint})}))
+      (group-by :input $)
+      (map (fn [[input results]]
+             (let [by-binary (into {} (map (juxt :bbin-binary identity) results))]
+               (merge
+                 {:child-dir (str (:child-dir (first results)))
+                  :0-input (with-meta input {:portal.viewer/default :portal.viewer/pprint})}
+                 (into {}
+                       (mapcat (fn [bbin-binary]
+                                 (let [result (by-binary bbin-binary)
+                                       ran (= (get-in result [:run :out]) "hello world\n")]
+                                   [[(keyword (str "1-" bbin-binary) "installed")
+                                     (if (:installed result) '✅ '❌)]
+                                    [(keyword (str "2-" bbin-binary) "ran")
+                                     (if ran '✅ '❌)]]))
+                               repl-bbin-binaries)))))
            $)
-      (sort-by :as $)
-      (with-meta $ {:portal.viewer/default :portal.viewer/table})))
-  (group-by #(if (:pass %) :pass :fail) run-results))
+      (sort-by :child-dir $)
+      (map #(dissoc % :child-dir) $)
+      (with-meta $ {:portal.viewer/default :portal.viewer/table}))))
