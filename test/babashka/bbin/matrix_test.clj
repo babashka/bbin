29ed21834dd33f8496f5fd2d778cac09189aaa77
Radford Smith <11401+rads@users.noreply.github.com>
2026-06-07 11:44:42 -0700

    Add matrix example install runner
    Extend the matrix test helpers to persist each generated input as input.edn so later install and run results can be traced back to the exact example configuration.
    
    Add install-examples! to walk child directories in deterministic basename order and run bbin install <child-dir> --as <prefix>-<basename> for each generated example, capturing either the install output or the thrown exception.
    
    Add run-examples! to execute successfully installed script aliases with babashka.process/shell and return structured data containing the alias, source directory, original input, install status, install message, and run output.

--- test/babashka/bbin/matrix_test.clj
+++ test/babashka/bbin/matrix_test.clj
@@ -1,6 +1,10 @@
 (ns babashka.bbin.matrix-test
-  (:require [babashka.fs :as fs]
-            [clojure.math.combinatorics :as combo]))
+  (:require [babashka.bbin.test-util :as tu]
+            [babashka.process :as p]
+            [babashka.fs :as fs]
+            [clojure.edn :as edn]
+            [clojure.math.combinatorics :as combo])
+  (:import (clojure.lang ExceptionInfo)))
 
 (def ^:private bbin-bin
   {'both-bb-edn-and-deps-edn {:main-opts ["-m" "main"]}})
@@ -42,6 +46,7 @@
                     {:example-dir example-dir})))
   (fs/create-dirs example-dir)
   (spit (fs/file example-dir "main.clj") main-clj)
+  (write-edn! (fs/file example-dir "input.edn") input)
   (when (contains? input :bb-edn)
     (write-edn! (fs/file example-dir "bb.edn")
                 (generated-bb-edn (:bb-edn input))))
@@ -50,10 +55,59 @@
                 (generated-deps-edn (:deps-edn input))))
   example-dir)
 
+(defn- child-dirs [parent-dir]
+  (->> (fs/list-dir parent-dir)
+       (filter fs/directory?)
+       (sort-by #(str (fs/file-name %)))))
+
+(defn install-examples! [parent-dir prefix]
+  (mapv (fn [child-dir]
+          (let [basename (fs/file-name child-dir)
+                script-name (str prefix "-" basename)]
+            {:child-dir child-dir
+             :as script-name
+             :input (edn/read-string (slurp (fs/file child-dir "input.edn")))
+             :out (try
+                    (tu/bbin ["install" (str child-dir) "--as" script-name]
+                             :out :string)
+                    (catch Exception e e))}))
+        (child-dirs parent-dir)))
+
+(defn run-examples! [install-results]
+  (->> install-results
+       (map (fn [{:keys [as child-dir input out]}]
+              (let [error (instance? ExceptionInfo out)
+                    run (when-not error
+                          (-> (p/shell as {:out :string :err :string :continue true})
+                              (select-keys [:out :err])))]
+                {:as as
+                 :child-dir (str child-dir)
+                 :input input
+                 :installed (not error)
+                 :install-message (if error (ex-message out) out)
+                 :run run})))))
+
 (comment
   (def repl-inputs (take 100 (all-possible-inputs)))
+  (def repl-prefix (str "test_" (System/nanoTime)))
   (map-indexed vector repl-inputs)
-  (let [prefix (str "test_" (System/nanoTime))]
-    (doseq [[i input] (map-indexed vector repl-inputs)
-            :let [example-dir (fs/file "target" prefix (str i))]]
-      (generate-example! example-dir input))))
+
+  (doseq [[i input] (map-indexed vector repl-inputs)
+          :let [example-dir (fs/file "target" repl-prefix (format "%02d" i))]]
+    (generate-example! example-dir input))
+
+  (def install-results (install-examples! (fs/file "target" repl-prefix)
+                                          repl-prefix))
+  (def run-results (run-examples! install-results))
+  (tap>
+    (as-> run-results $
+      (map (fn [{:keys [input run installed as]}]
+             (let [ran (= (:out run) "hello world\n")]
+               {:0-as (symbol as)
+                :1-installed (if installed '✅ '❌)
+                :2-ran (if ran '✅ '❌)
+                :3-input (with-meta input {:portal.viewer/default :portal.viewer/pprint})}))
+           $)
+      (sort-by :as $)
+      (with-meta $ {:portal.viewer/default :portal.viewer/table})))
+  (group-by #(if (:pass %) :pass :fail) run-results))
